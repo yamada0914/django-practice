ec2b4acab57b3bad403d9ea0ba7a401885e05789
wataru_ikeda <ikeda@strippers.jp>
2025-07-06 01:20:21 +0900

    トップページデザイン修正

--- base/views/item.py
+++ base/views/item.py
@@ -1,12 +1,43 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from base.models import Item, Category, Tag
+import csv
+import os
+from django.conf import settings
 
 
 class IndexListView(ListView):
     model = Item
     template_name = 'pages/index.html'
     queryset = Item.objects.filter(is_published=True)
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        context['CATEGORIES'] = range(1, 13)
+        # バナーCSV読み込み
+        banners = []
+        csv_path = os.path.join(settings.BASE_DIR, 'data/csv', 'banners.csv')
+        with open(csv_path, encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                banners.append(row)
+        context['BANNERS'] = banners
+        # POPULAR_ITEMSを直接取得し 12 件だけ使い 3 件ずつグループ化して 4 ページ表示
+        popular_items = list(Item.objects.filter(
+            is_published=True).order_by('-sold_count')[:12])
+        print('popular_items', popular_items)
+
+        def batch(lst, n):
+            return [lst[i:i+n] for i in range(0, len(lst), n)]
+        context['POPULAR_GROUPS'] = batch(popular_items, 3)
+        # 4 カラムバナー
+        context['FOUR_BANNERS'] = [
+            {"src": "https://hareruya2-filepool.s3.amazonaws.com/banner/webp/4column/img_2209_hare2supB.webp", "url": "#"},
+            {"src": "https://hareruya2-filepool.s3.amazonaws.com/banner/webp/4column/img_2211_hare2oripa.webp", "url": "#"},
+            {"src": "https://hareruya2-filepool.s3.amazonaws.com/banner/webp/4column/img_2209_hare2deckB.webp", "url": "#"},
+            {"src": "https://hareruya2-filepool.s3.amazonaws.com/banner/webp/4column/img_2209_hare2tumeB.webp", "url": "#"},
+        ]
+        return context
 
 
 class ItemDetailView(DetailView):
