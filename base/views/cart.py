f9cb7cdd0d134bd9550fcc7ac230f7a0cd60949c
wataru_ikeda <ikeda@strippers.jp>
2025-11-06 15:40:12 +0900

    カート画面修正

--- base/views/cart.py
+++ base/views/cart.py
@@ -4,67 +4,98 @@ from django.views.generic import View, ListView
 from base.models import Card
 from collections import OrderedDict
 
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.decorators import login_required
 
-
-class CartListView(LoginRequiredMixin, ListView):
+class CartListView(ListView):
     model = Card
     template_name = 'pages/cart.html'
 
-    def get_queryset(self):
-        cart = self.request.session.get('cart', None)
-        if cart is None or len(cart) == 0:
-            return redirect('/')
-        self.queryset = []
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.total = 0
+        self.tax_included_total = 0
+
+    def _get_cart_from_session(self):
+        """セッションからカートを取得"""
+        return self.request.session.get('cart', None)
+
+    def _is_cart_empty(self, cart):
+        """カートが空かどうかをチェック"""
+        return cart is None or len(cart.get('items', {})) == 0
+
+    def _build_cart_items(self, cart):
+        """カートアイテムを構築して queryset を作成"""
+        queryset = []
         for item_pk, quantity in cart['items'].items():
-            obj = Card.objects.get(pk=item_pk)
-            obj.quantity = quantity
-            obj.subtotal = int(obj.price * quantity)
-            self.queryset.append(obj)
-            self.total += obj.subtotal
+            try:
+                obj = Card.objects.get(pk=item_pk)
+                obj.quantity = quantity
+                obj.subtotal = int(obj.price * quantity)
+                queryset.append(obj)
+                self.total += obj.subtotal
+            except Card.DoesNotExist:
+                continue
+        return queryset
+
+    def _calculate_totals(self, cart):
+        """合計金額を計算してセッションに保存"""
         self.tax_included_total = int(self.total * (settings.TAX_RATE + 1))
         cart['total'] = self.total
         cart['tax_included_total'] = self.tax_included_total
         self.request.session['cart'] = cart
-        return super().get_queryset()
+
+    def get_queryset(self):
+        cart = self._get_cart_from_session()
+        self.total = 0
+        self.tax_included_total = 0
+
+        if self._is_cart_empty(cart):
+            return []
+
+        queryset = self._build_cart_items(cart)
+        self._calculate_totals(cart)
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        try:
-            context["total"] = self.total
-            context["tax_included_total"] = self.tax_included_total
-        except Exception:
-            pass
+        context["total"] = self.total
+        context["tax_included_total"] = self.tax_included_total
+        context["quantity_range"] = list(range(1, 21))
         return context
 
 
-class AddCartView(LoginRequiredMixin, View):
+class AddCartView(View):
 
-    # # getメソッドではトップへリダイレクトする場合はこのようにかけます。
-    # def get(self, request):
-    #     return redirect('/')
-
-    def post(self, request):
-        item_pk = request.POST.get('item_pk')
-        quantity = int(request.POST.get('quantity'))
+    def _get_or_create_cart(self, request):
+        """カートを取得または作成"""
         cart = request.session.get('cart', None)
         if cart is None or len(cart) == 0:
-            items = OrderedDict()
-            cart = {'items': items}
-        if item_pk in cart['items']:
+            cart = {'items': OrderedDict()}
+        return cart
+
+    def _update_cart_item(self, cart, item_pk, quantity, update):
+        """カートアイテムを更新"""
+        if update and item_pk in cart['items']:
+            cart['items'][item_pk] = quantity
+        elif item_pk in cart['items']:
             cart['items'][item_pk] += quantity
         else:
             cart['items'][item_pk] = quantity
+
+    def post(self, request):
+        item_pk = request.POST.get('item_pk')
+        quantity = int(request.POST.get('quantity', 1))
+        update = request.POST.get('update', 'false').lower() == 'true'
+
+        cart = self._get_or_create_cart(request)
+        self._update_cart_item(cart, item_pk, quantity, update)
         request.session['cart'] = cart
+
         return redirect('/cart/')
 
 
-@login_required
 def remove_from_cart(request, pk):
     cart = request.session.get('cart', None)
-    if cart is not None:
+    if cart is not None and pk in cart.get('items', {}):
         del cart['items'][pk]
         request.session['cart'] = cart
     return redirect('/cart/')
